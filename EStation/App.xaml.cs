e224bea2bd33acdf4c9b8207646ab308d5221ef9
Halid Cisse <halidoucse@hotmail.com>
2015-05-12 19:40:07 +0100

    _

---
+++ EStation/App.xaml.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security;
+using System.Windows;
+using System.Windows.Threading;
+using CLib;
+using CLib.Program;
+using EStationCore;
+using EStationCore.Model.Security.Entity;
+
+namespace EStation
+{
+
+    public partial class App 
+    {
+
+        App()
+        {
+            if (_enforcer.ShouldApplicationExit()) Shutdown();
+
+            try
+            {               
+                EStation = new CoreService();
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                Current.Shutdown();
+            }
+        }
+
+
+
+        #region DATA SERVICES
+
+        /// <summary>
+        /// Serveur de Donnees
+        /// </summary>
+        internal static CoreService EStation { get; private set; }
+
+
+        /// <summary>
+        /// L'utilisateur Actuelle
+        /// </summary>
+        internal static User CurrentUser;
+
+        #endregion
+
+
+
+        #region START UP EVENTS
+
+        /// <summary>
+        /// OnStartup
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+
+            base.OnStartup(e);
+            DispatcherUnhandledException += App_DispatcherUnhandledException;
+
+        }
+
+
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            var systemPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+
+            var directoryName = Path.Combine(systemPath, "Winxo");
+            var debugFile = Path.Combine(directoryName, "Debug.txt");
+
+            DebugHelper.Logger.SaveLog(debugFile);
+        }
+
+
+        // [DebuggerStepThrough]
+        static void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+
+            DebugHelper.Logger.WriteException(e.Exception);
+
+            if (e.Exception.GetType() == typeof(SecurityException))
+                MessageBox.Show("Permission Refusée");
+            else if (e.Exception.GetType() == typeof(InvalidOperationException))
+                MessageBox.Show(e.Exception.Message, "Not Handled Exception");
+            else if (e.Exception.GetType() == typeof(NullReferenceException))
+                DebugHelper.Logger.WriteException(e.Exception);
+            //MessageBox.Show(e.Exception.Message, "Not Handled Exception");
+            else if (e.Exception.GetType() == typeof(ArgumentNullException))
+                DebugHelper.Logger.WriteException(e.Exception);
+            //MessageBox.Show(e.Exception.Message, "Not Handled Exception");
+            else
+                MessageBox.Show(e.Exception.Message, "Not Handled Exception");
+
+
+            e.Handled = true;
+            //Current.Shutdown();
+        }
+
+        #endregion
+
+
+
+        #region SINGLE INSTANCE MEMBERS
+
+        /// <summary>
+        /// Verifier Q'une Seule Instance est Lancee
+        /// </summary>
+        readonly SingletonApplication.SingletonApplicationEnforcer _enforcer = new SingletonApplication.SingletonApplicationEnforcer(DisplayArgs);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        public static void DisplayArgs(IEnumerable<string> args)
+        {
+            var dispatcher = Current.Dispatcher;
+
+            if (dispatcher.CheckAccess()) ShowArgs();
+            else dispatcher.BeginInvoke(new Action(ShowArgs));
+
+        }
+
+        private static void ShowArgs()
+        {
+            var mainWindow = Current.MainWindow as MainWindow;
+            if (mainWindow?.WindowState == WindowState.Minimized)
+                mainWindow.WindowState = WindowState.Normal;
+            mainWindow?.Activate();
+        }
+
+        #endregion
+
+    }
+}
---
+++ EStation/MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using CLib;
+using EStation.Views;
+using EStation.Views.Common;
+using EStation.Views.Security;
+using EStationCore.Model.Security.Enums;
+
+namespace EStation
+{
+    
+
+    public partial class MainWindow 
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void _LOGOUT_BUTTON_OnClick(object sender, RoutedEventArgs e)
+        {
+            Thread.CurrentPrincipal = null;
+
+            new Task(() =>
+            {
+                Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    _MAIN_FRAME.Navigate(new Login());
+
+                    var cm = FindResource("UserPopup") as Popup;
+                    if (cm == null) return;
+                    cm.IsOpen = false;
+                }));
+            }).Start();
+
+        }
+
+
+        private void _POPUP_BUTTON_OnClick(object sender, RoutedEventArgs e)
+        {
+            var cm = FindResource("UserPopup") as Popup;
+            if (cm == null) return;
+            cm.PlacementTarget = sender as Button;
+            cm.IsOpen = true;
+        }
+
+
+        private void UserPopup_OnOpened(object sender, EventArgs e)
+        {
+            ((Popup)sender).DataContext = App.CurrentUser;
+
+            _USER_NAME_LABEL.Content = App.CurrentUser.FullName;
+        }
+
+
+        private void _ESPACES_LIST_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (((ListBox)(sender)).SelectedValue == null) return;
+
+            switch (((ListBox)(sender)).SelectedValue.ToString().ToEnum<UserSpace>())
+            {
+                case UserSpace.AdminSpace:
+                    new Task(() => Dispatcher.BeginInvoke(new Action(()
+                        => _MAIN_FRAME.NavigationService?.Navigate(new HomePage(), UriKind.Relative)))).Start();
+                    break;
+                //case UserSpace.SecretaireSpace:
+                //    new Task(() => Dispatcher.BeginInvoke(new Action(()
+                //        => _MAIN_FRAME.NavigationService?.Navigate(new TeacherSpace(), UriKind.Relative)))).Start();
+                //    break;
+                //case UserSpace.EconomatSpace:
+                //    new Task(() => Dispatcher.BeginInvoke(new Action(()
+                //        => _MAIN_FRAME.NavigationService?.Navigate(new EconomatView(false), UriKind.Relative)))).Start();
+                //    break;
+                //default:
+                //    new Task(() => Dispatcher.BeginInvoke(new Action(()
+                //        => _MAIN_FRAME.NavigationService?.Navigate(new Login(), UriKind.Relative)))).Start();
+                //    break;
+            }
+
+            var cm = FindResource("UserPopup") as Popup;
+            if (cm == null) return;
+            cm.IsOpen = false;
+        }
+
+
+        private void _CHANGE_PASS_OnClick(object sender, RoutedEventArgs e)
+        {
+            var wind = new ChangePass { Owner = GetWindow(this) };
+            wind.ShowDialog();
+        }
+
+
+
+
+
+
+
+
+    }
+}
